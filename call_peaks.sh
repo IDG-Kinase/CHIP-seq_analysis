5c1922f0e10cdccd42c82e3c6bae5a0edff05991
Matthew Berginski <matthew.berginski@gmail.com>
2021-03-06 14:37:26 -0500

    adding output to match (?) MACS calls from Goulet's paper

--- call_peaks.sh
+++ call_peaks.sh
@@ -19,26 +19,57 @@
 # 	--control results/bam_files/SUM229_pos_input_DMSO_1/Aligned.sortedByCoord.out.bam \
 # 	--outdir results/peak_calling/SUM229_Tram/ -n Tram
 
+# macs2 callpeak --treatment results/bam_files/SUM229_pos_Bort/Aligned.sortedByCoord.out.bam \
+# 	--control results/bam_files/SUM229_pos_DMSO/Aligned.sortedByCoord.out.bam \
+# 	--outdir results/peak_calling/SUM229_Bort_vs_DMSO/ -n Bort_gain
+# 
+# macs2 callpeak --treatment results/bam_files/SUM229_pos_DMSO/Aligned.sortedByCoord.out.bam \
+# 	--control results/bam_files/SUM229_pos_Bort/Aligned.sortedByCoord.out.bam \
+# 	--outdir results/peak_calling/SUM229_Bort_vs_DMSO/ -n Bort_lost
+# 
+# macs2 callpeak --treatment results/bam_files/SUM229_pos_MK2206/Aligned.sortedByCoord.out.bam \
+# 	--control results/bam_files/SUM229_pos_DMSO/Aligned.sortedByCoord.out.bam \
+# 	--outdir results/peak_calling/SUM229_MK2206_vs_DMSO/ -n MK2206_gain
+# 
+# macs2 callpeak --treatment results/bam_files/SUM229_pos_DMSO/Aligned.sortedByCoord.out.bam \
+# 	--control results/bam_files/SUM229_pos_MK2206/Aligned.sortedByCoord.out.bam \
+# 	--outdir results/peak_calling/SUM229_MK2206_vs_DMSO/ -n MK2206_lost
+# 
+# macs2 callpeak --treatment results/bam_files/SUM229_pos_Tram/Aligned.sortedByCoord.out.bam \
+# 	--control results/bam_files/SUM229_pos_DMSO/Aligned.sortedByCoord.out.bam \
+# 	--outdir results/peak_calling/SUM229_Tram_vs_DMSO/ -n Tram_gain
+# 
+# macs2 callpeak --treatment results/bam_files/SUM229_pos_DMSO/Aligned.sortedByCoord.out.bam \
+# 	--control results/bam_files/SUM229_pos_MK2206/Aligned.sortedByCoord.out.bam \
+# 	--outdir results/peak_calling/SUM229_Tram_vs_DMSO/ -n Tram_lost
+
 macs2 callpeak --treatment results/bam_files/SUM229_pos_Bort/Aligned.sortedByCoord.out.bam \
 	--control results/bam_files/SUM229_pos_DMSO/Aligned.sortedByCoord.out.bam \
-	--outdir results/peak_calling/SUM229_Bort_vs_DMSO/ -n Bort_gain
+	--outdir results/peak_calling/SUM229_Bort_vs_DMSO_Goulet/ -n Bort_gain\
+	--broad --max-gap 1000
 
 macs2 callpeak --treatment results/bam_files/SUM229_pos_DMSO/Aligned.sortedByCoord.out.bam \
 	--control results/bam_files/SUM229_pos_Bort/Aligned.sortedByCoord.out.bam \
-	--outdir results/peak_calling/SUM229_Bort_vs_DMSO/ -n Bort_lost
+	--outdir results/peak_calling/SUM229_Bort_vs_DMSO_Goulet/ -n Bort_lost \
+	--broad --max-gap 1000
+
 
 macs2 callpeak --treatment results/bam_files/SUM229_pos_MK2206/Aligned.sortedByCoord.out.bam \
 	--control results/bam_files/SUM229_pos_DMSO/Aligned.sortedByCoord.out.bam \
-	--outdir results/peak_calling/SUM229_MK2206_vs_DMSO/ -n MK2206_gain
+	--outdir results/peak_calling/SUM229_MK2206_vs_DMSO_Goulet/ -n MK2206_gain \
+	--broad --max-gap 1000
 
 macs2 callpeak --treatment results/bam_files/SUM229_pos_DMSO/Aligned.sortedByCoord.out.bam \
 	--control results/bam_files/SUM229_pos_MK2206/Aligned.sortedByCoord.out.bam \
-	--outdir results/peak_calling/SUM229_MK2206_vs_DMSO/ -n MK2206_lost
+	--outdir results/peak_calling/SUM229_MK2206_vs_DMSO_Goulet/ -n MK2206_lost \
+	--broad --max-gap 1000
 
 macs2 callpeak --treatment results/bam_files/SUM229_pos_Tram/Aligned.sortedByCoord.out.bam \
 	--control results/bam_files/SUM229_pos_DMSO/Aligned.sortedByCoord.out.bam \
-	--outdir results/peak_calling/SUM229_Tram_vs_DMSO/ -n Tram_gain
+	--outdir results/peak_calling/SUM229_Tram_vs_DMSO_Goulet/ -n Tram_gain \
+	--broad --max-gap 1000
 
 macs2 callpeak --treatment results/bam_files/SUM229_pos_DMSO/Aligned.sortedByCoord.out.bam \
 	--control results/bam_files/SUM229_pos_MK2206/Aligned.sortedByCoord.out.bam \
-	--outdir results/peak_calling/SUM229_Tram_vs_DMSO/ -n Tram_lost
+	--outdir results/peak_calling/SUM229_Tram_vs_DMSO_Goulet/ -n Tram_lost \
+	--broad --max-gap 1000
